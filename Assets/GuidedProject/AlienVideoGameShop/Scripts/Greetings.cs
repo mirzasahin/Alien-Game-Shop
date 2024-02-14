6353fae0b21369433cafb4f34a685c622eb0242e
Mirza Sahin <salihmirza123@gmail.com>
2024-02-15 00:14:12 +0300

    Add 3d models and update particular system

---
+++ Assets/GuidedProject/AlienVideoGameShop/Scripts/Greetings.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Greetings : MonoBehaviour
+{
+    public List<AudioClip> greetings = new List<AudioClip>();
+
+    public AudioSource source;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        AudioClip clip = greetings[Random.Range(0, greetings.Count - 1)];
+        source.PlayOneShot(clip);
+    }
+}
